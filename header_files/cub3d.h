f9014b90e999ad5dc54f8c846bdbd502da5f4e4f
Napoleon Farfetch <nfarfetc@va-e3.kzn.21-school.ru>
2022-07-08 15:51:18 +0300

    IDK

--- header_files/cub3d.h
+++ header_files/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: nfarfetc <nfarfetc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:39:53 by nfarfetc          #+#    #+#             */
-/*   Updated: 2022/07/07 19:10:42 by nfarfetc         ###   ########.fr       */
+/*   Updated: 2022/07/08 15:50:40 by nfarfetc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,31 @@
 # define WIN_LENGTH 1920
 # define WIN_WIDTH	1080
 
+# define NORTH 2
+# define SOUTH 3
+# define WEST 4
+# define EAST 5
+
 # define FLOOR 0
 # define WALL 1
 # define EMPTY -1
 
 # define EXIT_KEY 53
+
+typedef struct s_rays
+{
+	double	pos_x;
+	double	pos_y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+	double	camera_x;
+	double	ray_dir_x;
+	double	ray_dir_y;
+	int		map_x;
+	int		map_y;
+}	t_rays;
 
 typedef struct s_cub
 {
@@ -36,10 +56,9 @@ typedef struct s_cub
 	void	*mlx_win;
 	int		map_length;
 	int		map_width;
-	int		pos_x;
-	int		pos_y;
 	int		**map;
 	char	*map_path;
+	t_rays	*rays;
 }	t_cub;
 
 /* Initialization */
--- source_files/initialization.c
+++ source_files/initialization.c
@@ -6,7 +6,7 @@
 /*   By: nfarfetc <nfarfetc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 16:26:17 by nfarfetc          #+#    #+#             */
-/*   Updated: 2022/07/07 19:12:14 by nfarfetc         ###   ########.fr       */
+/*   Updated: 2022/07/08 15:30:14 by nfarfetc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,24 @@ static int	**get_map(int length, int width)
 	return (map);
 }
 
+t_rays	*rays_init(t_cub *cub)
+{
+	t_rays	*rays;
+
+	rays = (t_rays *)malloc(sizeof(t_rays));
+	if (rays == NULL)
+		return (NULL);
+	rays->pos_x = 50; /*Change after parsing*/
+	rays->pos_y = 50; /*Change after parsing*/
+	rays->dir_x = -1;
+	rays->dir_y = 0;
+	rays->plane_x = 0;
+	rays->plane_y = 0.66;
+	rays->camera_x = 0;
+	rays->ray_dir_x = 0;
+	rays->ray_dir_y = 0;
+}
+
 t_cub	*cub_init(char *map_path)
 {
 	t_cub	*cub;
@@ -73,12 +91,11 @@ t_cub	*cub_init(char *map_path)
 		return (NULL);
 	cub->map_length = 100; /*Change after parsing*/
 	cub->map_width = 100; /*Change after parsing*/
-	cub->pos_x = 50; /*Change after parsing*/
-	cub->pos_y = 50; /*Change after parsing*/
 	cub->map = get_map(cub->map_length, cub->map_width);
 	if (cub->map == NULL)
 		return (NULL);
-	fill_map(cub);
 	cub->map_path = map_path;
+	fill_map(cub);
+	cub->rays = rays_init(cub);
 	return (cub);
 }
